97c3744fd6947c355d6d60a4bc4ffac83699cb68
Gremious Grenald <gremious@gremy.co.uk>
2025-04-15 21:03:08 +0100

    Initial Commit

---
+++ src/client/java/uk/co/gremy/CreateharderClient.java
@@ -0,0 +1,10 @@
+package uk.co.gremy;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CreateharderClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/client/java/uk/co/gremy/CreateharderDataGenerator.java
@@ -0,0 +1,11 @@
+package uk.co.gremy;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class CreateharderDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
